7aa7b83677f5b75a2a754a1d831f715d3404c16b
songtomtom <tom.song@luxrobo.com>
2022-06-19 17:16:16 +0900

    디비 콘피그 생성

--- main.go
+++ main.go
@@ -1,20 +1,5 @@
 package main
 
-import (
-	"fmt"
-	"log"
-	"net/http"
-)
-
 func main() {
-	http.HandleFunc("/", func(rw http.ResponseWriter, r *http.Request) {
-		fmt.Fprint(rw, "Hello World")
-	})
-
-	http.HandleFunc("/bar", func(rw http.ResponseWriter, r *http.Request) {
-		fmt.Fprint(rw, "Hello Bar")
-	})
-
-	log.Fatal(http.ListenAndServe(":3000", nil))
 
 }
