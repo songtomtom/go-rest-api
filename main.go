1fd6958e5f05d42ab0c36d161ff496cdde7f351c
songtomtom <tom.song@luxrobo.com>
2022-06-19 16:34:59 +0900

    처음부터 시작

--- main.go
+++ main.go
@@ -1,29 +1,20 @@
 package main
 
 import (
+	"fmt"
+	"log"
 	"net/http"
 )
 
-var users = map[string]*User{}
-
-type User struct {
-	id   string "id"
-	name string "name"
-}
-
 func main() {
-
 	http.HandleFunc("/", func(rw http.ResponseWriter, r *http.Request) {
-		// rw.Write([]byte("hello"))
-
-		switch r.Method {
-		case http.MethodGet:
-
-			// json.NewEncod zer(rw).Encode()
-
-		}
-
+		fmt.Fprint(rw, "Hello World")
 	})
-	http.ListenAndServe(":8080", nil)
+
+	http.HandleFunc("/bar", func(rw http.ResponseWriter, r *http.Request) {
+		fmt.Fprint(rw, "Hello Bar")
+	})
+
+	log.Fatal(http.ListenAndServe(":3000", nil))
 
 }
