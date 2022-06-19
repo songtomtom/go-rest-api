95fa4c292fd7ad0e3aab673064a3f9f2d2ced26b
songtomtom <tom.song@luxrobo.com>
2022-06-19 15:49:21 +0900

    타입지정

--- main.go
+++ main.go
@@ -1,11 +1,28 @@
 package main
 
-import "net/http"
+import (
+	"net/http"
+)
+
+var users = map[string]*User{}
+
+type User struct {
+	id   string "id"
+	name string "name"
+}
 
 func main() {
 
 	http.HandleFunc("/", func(rw http.ResponseWriter, r *http.Request) {
-		rw.Write([]byte("hello"))
+		// rw.Write([]byte("hello"))
+
+		switch r.Method {
+		case http.MethodGet:
+
+			// json.NewEncod zer(rw).Encode()
+
+		}
+
 	})
 	http.ListenAndServe(":8080", nil)
 
