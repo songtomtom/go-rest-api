1068e456f65599920db7e32e3898be6543d4af5f
songtomtom <tom.song@luxrobo.com>
2022-06-19 20:46:00 +0900

    모델 타입 정의중

--- app/model/model.go
+++ app/model/model.go
@@ -1,1 +1,23 @@
 package model
+
+import (
+	"time"
+
+	"gorm.io/gorm"
+)
+
+type Project struct {
+	gorm.Model
+	Title    string `json:"title"`
+	Archived bool   `json:"archived"`
+	Tasks    []Task `json:"tasks"`
+}
+
+type Task struct {
+	gorm.Model
+	Title     string     `json:"title"`
+	Priority  string     `json:"priority"`
+	Deadline  *time.Time `json:"deadline"`
+	Done      bool       `json:"done"`
+	ProjectId uint       `json:"project_id"`
+}
