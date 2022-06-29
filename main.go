604d984bbce13e3dd71202660b6913e3bbd3093c
songtomtom <tom.song@luxrobo.com>
2022-06-29 11:47:53 +0900

    디비연결 성공

--- main.go
+++ main.go
@@ -1,5 +1,15 @@
 package main
 
+import (
+	"gorm.io/driver/mysql"
+	"gorm.io/gorm"
+)
+
 func main() {
+	dsn := "root:qweqweqwe@tcp(127.0.0.1:3306)/go_rest_api"
+	_, err := gorm.Open(mysql.Open(dsn), &gorm.Config{})
+	if err != nil {
+		panic("Db 연결에 실패하였습니다.")
+	}
 
 }
